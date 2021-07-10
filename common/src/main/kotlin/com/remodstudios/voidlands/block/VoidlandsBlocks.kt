5ff19ed008f33ca54d81d947772a738fe67a3806
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-07-10 19:24:33 +0300

    Saffron => Crayola

--- common/src/main/kotlin/com/remodstudios/voidlands/block/VoidlandsBlocks.kt
+++ common/src/main/kotlin/com/remodstudios/voidlands/block/VoidlandsBlocks.kt
@@ -26,19 +26,19 @@ object VoidlandsBlocks : BlockRegistryHelper(Voidlands.MOD_ID) {
     val DARK_RED_TERRACOTTA = addCopy("dark_red_terracotta", Blocks.BLACK_TERRACOTTA)
     val DARK_RED_WOOL = addCopy("dark_red_wool", Blocks.BLACK_WOOL)
 
-    val SAFFRON_CANDLE = add("saffron_candle", CandleBlock(BlockProperties.copy(Blocks.CANDLE)))
-    val SAFFRON_CANDLE_CAKE = add("saffron_candle_cake",
-        ModCandleCakeBlock(SAFFRON_CANDLE, BlockProperties.copy(Blocks.CANDLE_CAKE)))
-    val SAFFRON_CONCRETE = addCopy("saffron_concrete", Blocks.BLACK_CONCRETE)
-    val SAFFRON_CONCRETE_POWDER = add("saffron_concrete_powder",
-        ConcretePowderBlock(SAFFRON_CONCRETE, BlockProperties.copy(Blocks.BLACK_CONCRETE_POWDER)))
-    val SAFFRON_GLAZED_TERRACOTTA = add("saffron_glazed_terracotta",
+    val CRAYOLA_CANDLE = add("crayola_candle", CandleBlock(BlockProperties.copy(Blocks.CANDLE)))
+    val CRAYOLA_CANDLE_CAKE = add("crayola_candle_cake",
+        ModCandleCakeBlock(CRAYOLA_CANDLE, BlockProperties.copy(Blocks.CANDLE_CAKE)))
+    val CRAYOLA_CONCRETE = addCopy("crayola_concrete", Blocks.BLACK_CONCRETE)
+    val CRAYOLA_CONCRETE_POWDER = add("crayola_concrete_powder",
+        ConcretePowderBlock(CRAYOLA_CONCRETE, BlockProperties.copy(Blocks.BLACK_CONCRETE_POWDER)))
+    val CRAYOLA_GLAZED_TERRACOTTA = add("crayola_glazed_terracotta",
         GlazedTerracottaBlock(BlockProperties.copy(Blocks.BLACK_GLAZED_TERRACOTTA)))
-    val SAFFRON_SHULKER_BOX = add("saffron_shulker_box", Block(BlockProperties.copy(Blocks.BLACK_SHULKER_BOX))) // TODO custom shulker box??
-    val SAFFRON_STAINED_GLASS = add("saffron_stained_glass", GlassBlock(BlockProperties.copy(Blocks.BLACK_STAINED_GLASS)))
-    val SAFFRON_STAINED_GLASS_PANE = add("saffron_stained_glass_pane", ModPaneBlock(BlockProperties.copy(Blocks.BLACK_STAINED_GLASS_PANE)))
-    val SAFFRON_TERRACOTTA = addCopy("saffron_terracotta", Blocks.BLACK_TERRACOTTA)
-    val SAFFRON_WOOL = addCopy("saffron_wool", Blocks.BLACK_WOOL)
+    val CRAYOLA_SHULKER_BOX = add("crayola_shulker_box", Block(BlockProperties.copy(Blocks.BLACK_SHULKER_BOX))) // TODO custom shulker box??
+    val CRAYOLA_STAINED_GLASS = add("crayola_stained_glass", GlassBlock(BlockProperties.copy(Blocks.BLACK_STAINED_GLASS)))
+    val CRAYOLA_STAINED_GLASS_PANE = add("crayola_stained_glass_pane", ModPaneBlock(BlockProperties.copy(Blocks.BLACK_STAINED_GLASS_PANE)))
+    val CRAYOLA_TERRACOTTA = addCopy("crayola_terracotta", Blocks.BLACK_TERRACOTTA)
+    val CRAYOLA_WOOL = addCopy("crayola_wool", Blocks.BLACK_WOOL)
 
     val ASHSTONE = addCopy("ashstone", Blocks.NETHER_BRICKS)
     val ASHSTONE_BRICKS = addCopy("ashstone_bricks", Blocks.NETHER_BRICKS)
@@ -59,7 +59,7 @@ object VoidlandsBlocks : BlockRegistryHelper(Voidlands.MOD_ID) {
             CANNA, CANNA_SPROUT)
         RenderTypeRegistry.register(RenderLayer.getTranslucent(),
             DARK_RED_STAINED_GLASS, DARK_RED_STAINED_GLASS_PANE,
-            SAFFRON_STAINED_GLASS, SAFFRON_STAINED_GLASS_PANE,
+            CRAYOLA_STAINED_GLASS, CRAYOLA_STAINED_GLASS_PANE,
             DUST_CLOUD)
     }
 }
