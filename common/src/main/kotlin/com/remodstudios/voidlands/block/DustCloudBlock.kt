d0f7d040c4ed84228a3d0a075f00774625feb1a8
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-07-27 17:46:11 +0300

    Allow mobs to pathfind through dust clouds

--- common/src/main/kotlin/com/remodstudios/voidlands/block/DustCloudBlock.kt
+++ common/src/main/kotlin/com/remodstudios/voidlands/block/DustCloudBlock.kt
@@ -3,8 +3,10 @@ package com.remodstudios.voidlands.block
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
 import net.minecraft.entity.Entity
+import net.minecraft.entity.ai.pathing.NavigationType
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Vec3d
+import net.minecraft.world.BlockView
 import net.minecraft.world.World
 
 class DustCloudBlock(settings: Settings?) : Block(settings) {
@@ -14,5 +16,15 @@ class DustCloudBlock(settings: Settings?) : Block(settings) {
 
     override fun onEntityCollision(blockState: BlockState?, world: World?, blockPos: BlockPos?, entity: Entity) {
         entity.slowMovement(blockState, SLOW_FACTOR)
+    }
+
+    override fun canPathfindThrough(
+        state: BlockState,
+        world: BlockView,
+        pos: BlockPos,
+        navType: NavigationType
+    ): Boolean {
+        // pretend we're air
+        return true
     }
 }
