158e7f621cdbbe8b2eb44b3afb9e0d2b2c25ebc4
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-07-28 14:31:37 +0300

    Experiment with Dust Cloud side visibility

--- common/src/main/kotlin/com/remodstudios/voidlands/block/DustCloudBlock.kt
+++ common/src/main/kotlin/com/remodstudios/voidlands/block/DustCloudBlock.kt
@@ -1,15 +1,18 @@
 package com.remodstudios.voidlands.block
 
-import net.minecraft.block.Block
 import net.minecraft.block.BlockState
+import net.minecraft.block.ShapeContext
+import net.minecraft.block.TransparentBlock
 import net.minecraft.entity.Entity
 import net.minecraft.entity.ai.pathing.NavigationType
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Vec3d
+import net.minecraft.util.shape.VoxelShape
+import net.minecraft.util.shape.VoxelShapes
 import net.minecraft.world.BlockView
 import net.minecraft.world.World
 
-class DustCloudBlock(settings: Settings) : Block(settings) {
+class DustCloudBlock(settings: Settings) : TransparentBlock(settings) {
     companion object {
         private val SLOW_FACTOR = Vec3d(1.0, 0.05000000074505806, 1.0)
     }
@@ -27,4 +30,12 @@ class DustCloudBlock(settings: Settings) : Block(settings) {
         // pretend we're air
         return true
     }
+
+    override fun getCameraCollisionShape(state: BlockState, world: BlockView, pos: BlockPos, ctx: ShapeContext): VoxelShape =
+        VoxelShapes.empty()
+
+    override fun getAmbientOcclusionLightLevel(state: BlockState, world: BlockView, pos: BlockPos) =
+        1.0f
+
+    override fun isTranslucent(state: BlockState, world: BlockView, pos: BlockPos): Boolean = true
 }
